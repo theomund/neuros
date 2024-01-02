07c331d2026f26602b4406d4d3ff8c155ce8311f
Theomund <34360334+Theomund@users.noreply.github.com>
2024-01-01 22:36:45 -0500

    feat: add basic graphical environment

--- Makefile
+++ Makefile
@@ -1,13 +1,47 @@
 SHELL := /bin/sh
 
-.PHONY: build
-build:
-	$(MAKE) -C kernel build
+NAME := NeurOS
+
+$(NAME).iso: limine kernel
+	mkdir -p iso_root/EFI/BOOT
+	cp -v bootloader/limine.cfg kernel/kernel.elf limine/limine-bios.sys limine/limine-bios-cd.bin limine/limine-uefi-cd.bin iso_root/
+	cp -v limine/BOOTX64.EFI limine/BOOTIA32.EFI iso_root/EFI/BOOT/
+	xorriso -as mkisofs -b limine-bios-cd.bin \
+		-no-emul-boot -boot-load-size 4 -boot-info-table \
+		--efi-boot limine-uefi-cd.bin \
+		-efi-boot-part --efi-boot-image --protective-msdos-label \
+		iso_root -o $(NAME).iso
+	./limine/limine bios-install $(NAME).iso
+	rm -rf iso_root
+
+limine:
+	git clone https://github.com/limine-bootloader/limine.git --branch=v6.x-branch-binary --depth=1
+	$(MAKE) -C limine
+
+.PHONY: all
+all: $(NAME).iso
 
 .PHONY: clean
 clean:
 	$(MAKE) -C kernel clean
+	rm -f $(NAME).iso
+
+.PHONY: distclean
+distclean: clean
+	rm -rf limine ovmf
+
+.PHONY: format
+format:
+	$(MAKE) -C kernel format
+
+.PHONY: kernel
+kernel:
+	$(MAKE) -C kernel
 
 .PHONY: lint
 lint:
 	$(MAKE) -C kernel lint
+
+.PHONY: run
+run: $(NAME).iso
+	qemu-system-x86_64 -M q35 -m 2G -cdrom $(NAME).iso -boot d
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::arch::asm;
+
+static FRAMEBUFFER_REQUEST: limine::FramebufferRequest = limine::FramebufferRequest::new(0);
+static BASE_REVISION: limine::BaseRevision = limine::BaseRevision::new(0);
+
+#[no_mangle]
+extern "C" fn _start() -> ! {
+    assert!(BASE_REVISION.is_supported());
+
+    if let Some(framebuffer_response) = FRAMEBUFFER_REQUEST.get_response().get() {
+        if framebuffer_response.framebuffer_count < 1 {
+            hcf();
+        }
+
+        let framebuffer = &framebuffer_response.framebuffers()[0];
+
+        for i in 0..100_usize {
+            let pixel_offset = i * framebuffer.pitch as usize + i * 4;
+            let address = framebuffer
+                .address
+                .as_ptr()
+                .unwrap()
+                .wrapping_add(pixel_offset) as *mut u32;
+            unsafe {
+                *(address) = 0xFFFFFFFF;
+            }
+        }
+    }
+
+    hcf();
+}
+
+#[panic_handler]
+fn rust_panic(_info: &core::panic::PanicInfo) -> ! {
+    hcf();
+}
+
+fn hcf() -> ! {
+    unsafe {
+        asm!("cli");
+        loop {
+            asm!("hlt");
+        }
+    }
 }
