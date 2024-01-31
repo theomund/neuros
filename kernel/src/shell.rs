412b4ffbb21709702e5b18101ec218377d471345
Theomund <34360334+Theomund@users.noreply.github.com>
2024-01-30 19:10:18 -0500

    feat: add echo and help command

--- kernel/src/shell.rs
+++ kernel/src/shell.rs
@@ -15,29 +15,54 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 use crate::serial::SERIAL;
+use alloc::string::String;
 use alloc::vec::Vec;
 use core::fmt::{Result, Write};
 
 const BLUE: &str = "\x1b[38;2;0;151;230m";
 const BOLD: &str = "\x1b[1m";
 const DEFAULT: &str = "\x1b[39m";
+const NORMAL: &str = "\x1b[0m";
 const RED: &str = "\x1b[38;2;232;65;24m";
 
 pub fn initialize() -> Result {
     let version = env!("CARGO_PKG_VERSION");
     let author = env!("CARGO_PKG_AUTHORS");
     write!(SERIAL.lock(), "{}", BOLD)?;
-    write!(SERIAL.lock(), "{}NeurOS v{} (x86_64)\n\r", RED, version)?;
-    write!(SERIAL.lock(), "{}Copyright (C) 2024 {}\n\n\r", BLUE, author)?;
-    write!(SERIAL.lock(), "{}> ", DEFAULT)?;
+    writeln!(SERIAL.lock(), "{}NeurOS v{} (x86_64)", RED, version)?;
+    writeln!(SERIAL.lock(), "\r{}Copyright (C) 2024 {}", BLUE, author)?;
+    write!(SERIAL.lock(), "\n\r{}> ", DEFAULT)?;
     let mut buffer: Vec<char> = Vec::new();
     loop {
         let character = SERIAL.lock().read() as char;
         match character {
             '\r' => {
+                let line: String = buffer.iter().collect();
+                let input = line.split_once(char::is_whitespace);
+                let command = match input {
+                    Some(pair) => pair.0,
+                    None => line.as_str(),
+                };
+                writeln!(SERIAL.lock(), "{}", NORMAL)?;
+                match command {
+                    "echo" => {
+                        let argument = match input {
+                            Some(pair) => pair.1,
+                            None => "",
+                        };
+                        writeln!(SERIAL.lock(), "\r{}", argument)?;
+                    }
+                    "help" => {
+                        writeln!(SERIAL.lock(), "\rAvailable commands:")?;
+                        writeln!(SERIAL.lock(), "\r\techo -- Display a line of text.")?;
+                        writeln!(SERIAL.lock(), "\r\thelp -- Print a list of commands.")?;
+                    }
+                    _ => {
+                        writeln!(SERIAL.lock(), "\r{}ERROR: Command not found.", RED)?;
+                    }
+                }
+                write!(SERIAL.lock(), "\r{}{}> ", BOLD, DEFAULT)?;
                 buffer.clear();
-                write!(SERIAL.lock(), "\n\r{}ERROR: Command not found.\n\r", RED)?;
-                write!(SERIAL.lock(), "{}> ", DEFAULT)?;
             }
             '\x08' => {
                 if !buffer.is_empty() {
