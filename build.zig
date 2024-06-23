29d07c0854fd1f61e08e162f9b0271bc651918fd
Theomund <34360334+Theomund@users.noreply.github.com>
2024-06-23 06:10:13 -0400

    feat: add new build steps

--- build.zig
+++ build.zig
@@ -7,16 +7,18 @@ pub fn build(b: *std.Build) void {
         .abi = .none,
     };
 
-    const Features = std.Target.x86.Feature;
-    target.cpu_features_sub.addFeature(@intFromEnum(Features.mmx));
-    target.cpu_features_sub.addFeature(@intFromEnum(Features.sse));
-    target.cpu_features_sub.addFeature(@intFromEnum(Features.sse2));
-    target.cpu_features_sub.addFeature(@intFromEnum(Features.avx));
-    target.cpu_features_sub.addFeature(@intFromEnum(Features.avx2));
-    target.cpu_features_add.addFeature(@intFromEnum(Features.soft_float));
+    const features = std.Target.x86.Feature;
+    target.cpu_features_sub.addFeature(@intFromEnum(features.mmx));
+    target.cpu_features_sub.addFeature(@intFromEnum(features.sse));
+    target.cpu_features_sub.addFeature(@intFromEnum(features.sse2));
+    target.cpu_features_sub.addFeature(@intFromEnum(features.avx));
+    target.cpu_features_sub.addFeature(@intFromEnum(features.avx2));
+    target.cpu_features_add.addFeature(@intFromEnum(features.soft_float));
 
     const optimize = b.standardOptimizeOption(.{});
+
     const limine = b.dependency("limine", .{});
+
     const kernel = b.addExecutable(.{
         .name = "kernel",
         .root_source_file = .{ .path = "src/kernel/main.zig" },
@@ -25,11 +27,18 @@ pub fn build(b: *std.Build) void {
         .code_model = .kernel,
         .pic = true,
     });
-
     kernel.root_module.addImport("limine", limine.module("limine"));
     kernel.setLinkerScriptPath(.{ .path = "src/kernel/linker.ld" });
-
     kernel.want_lto = false;
 
     b.installArtifact(kernel);
+
+    const kernel_step = b.step("kernel", "Build the kernel");
+    kernel_step.dependOn(&kernel.step);
+
+    const iso_step = b.step("iso", "Build the ISO");
+    iso_step.dependOn(kernel_step);
+
+    const run_step = b.step("run", "Run the operating system");
+    run_step.dependOn(iso_step);
 }
