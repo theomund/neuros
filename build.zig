547c1c49c330bb59b9dca69ce035492f697dee69
Theomund <34360334+Theomund@users.noreply.github.com>
2024-06-23 07:16:52 -0400

    fix: address kernel build issues

--- build.zig
+++ build.zig
@@ -21,14 +21,14 @@ pub fn build(b: *std.Build) void {
 
     const kernel = b.addExecutable(.{
         .name = "kernel",
-        .root_source_file = .{ .path = "src/kernel/main.zig" },
+        .root_source_file = b.path("src/kernel/main.zig"),
         .target = b.resolveTargetQuery(target),
         .optimize = optimize,
         .code_model = .kernel,
         .pic = true,
     });
     kernel.root_module.addImport("limine", limine.module("limine"));
-    kernel.setLinkerScriptPath(.{ .path = "src/kernel/linker.ld" });
+    kernel.setLinkerScriptPath(b.path("src/kernel/linker.ld"));
     kernel.want_lto = false;
 
     b.installArtifact(kernel);
